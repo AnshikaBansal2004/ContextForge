b5bfe26bdcbfdd57e2a8b093405956d214449367
AnshikaBansal2004 <anshikabansal917@gmail.com>
2026-06-29 22:18:15 +0530

    adding create-user tool and user db

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,42 @@
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import {McpServer} from "@modelcontextprotocol/sdk/server/mcp.js";
+import {z} from "zod";
 
 // name = the type of tool u will be catering to 
 const server = new McpServer({
       name : "test",
       version : "1.0.0",
 })
+
+//creating a tool 
+server.tool(
+  "create-user",
+  "Creates a user in the database",
+  {
+    name: z.string(),
+    email: z.string(),
+    address: z.string(),
+    phone: z.string(),
+  },
+  {
+      //anotations for the AI feedback? 
+    title: "Create User",
+    readOnlyHint: false,
+    destructiveHint: false,
+    idempotentHint: false,
+    openWorldHint: true,
+  },
+  async ({ name, email, address, phone }) => {
+    return {
+      content: [
+        {
+          type: "text",
+          text: `Created user ${name}`,
+        },
+      ],
+    };
+  }
+);
 
 async function main(){
       const transport = new StdioServerTransport();
